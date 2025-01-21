42f7c85ddbfd7fdb0d7babbae4130df94f5644a3
Alex He <donut11one@gmail.com>
2025-01-21 09:17:01 -0800

    added basic frame to program

---
+++ JobAssignmentFinder.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class JobAssignmentFinder {
+
+    public void readDataFile(String s){
+
+    }
+
+    public int getInputSize(){
+        return 0;
+    }
+
+    public int[][] getBenefitMatrix(){
+        int x[][] = {{1,2},{3,4}};
+        return x;
+    }
+
+    public String benefitMatrixToString(){
+        return"some matrix";
+    }
+
+    public ArrayList<Integer>getMaxAssignment(){
+        ArrayList<Integer> thisList = new ArrayList<>();
+
+        return thisList;
+    }
+
+    public int getMaxAssignmentTotalValue(){
+        return 0;
+    }
+    
+    public int getBenefit(int x,int y){
+        return 0;
+    }
+
+
+}
